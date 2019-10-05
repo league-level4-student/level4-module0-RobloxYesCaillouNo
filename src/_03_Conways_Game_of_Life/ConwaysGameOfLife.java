a40ec5d75db0544ce7a4d9e2b6aeeb368139bad8
League Student <league@iMac-23.attlocal.net>
2019-10-05 14:35:19 -0700

    Saves

--- src/_03_Conways_Game_of_Life/ConwaysGameOfLife.java
+++ src/_03_Conways_Game_of_Life/ConwaysGameOfLife.java
@@ -1,97 +1,181 @@
 package _03_Conways_Game_of_Life;
-import java.awt.BorderLayout;
+
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.Timer;
 
-public class ConwaysGameOfLife extends JPanel implements ActionListener{
+public class WorldPanel extends JPanel implements MouseListener, ActionListener {
 	private static final long serialVersionUID = 1L;
-	
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 500;
-	public static final int CELLS_PER_ROW = 50;
-	
-	private JFrame window;
-	private JPanel inputPanel;
-	private JButton startStopButton;
-	private JButton randomizeButton;
-	private JButton clearButton;
-	private JLabel speedLabel;
-	private JTextField speedField;
-	
-	private WorldPanel gamePanel;
-	
-	public static void main(String[] args) {
-		new ConwaysGameOfLife().launchGame();
-	}
-	
-	public void launchGame() {
-		//build the window and start the simulation
-		setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		setLayout(new BorderLayout());
-		
-		window = new JFrame("Conway's Game Of Life");
-		window.add(this);
-		
-		inputPanel = new JPanel();
-		startStopButton = new JButton("START");
-		startStopButton.addActionListener(this);
-		randomizeButton = new JButton("RANDOMIZE");
-		randomizeButton.addActionListener(this);
-		clearButton = new JButton("CLEAR");
-		clearButton.addActionListener(this);
-		speedLabel = new JLabel("delay:");
-		speedField = new JTextField(5);
-		speedField.setText(Integer.toString(60));
-		
-		inputPanel.add(startStopButton);
-		inputPanel.add(speedLabel);
-		inputPanel.add(speedField);
-		inputPanel.add(randomizeButton);
-		inputPanel.add(clearButton);
-		add(inputPanel, BorderLayout.NORTH);
+	private int cellsPerRow;
+	private int cellSize;
 
-		gamePanel = new WorldPanel(WIDTH, HEIGHT, CELLS_PER_ROW);
-		add(gamePanel, BorderLayout.CENTER);
-		
-		window.pack();
-		window.setResizable(false);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
-	}
-	boolean run = false;
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == startStopButton) {
-			run = !run;
-			if(run) {
-				gamePanel.startAnimation();
-				startStopButton.setText("STOP");
-				int x = 200;
-				try {
-					x = Integer.parseInt(speedField.getText());
-					if(x <= 0) {
-						x = 1;
-					}
-				}catch (NumberFormatException ex) {
-					ex.printStackTrace();
-				}
-				gamePanel.setAnimationDelay(x);
-			}else {
-				gamePanel.stopAnimation();
-				startStopButton.setText("START");
+	private Timer timer;
+
+	// 1. Create a 2D array of Cells. Do not initialize it.
+
+	Cell[][] cells;
+
+	public WorldPanel(int w, int h, int cpr) {
+		setPreferredSize(new Dimension(w, h));
+		addMouseListener(this);
+		timer = new Timer(500, this);
+		this.cellsPerRow = cpr;
+
+		// 2. Calculate the cell size.
+		cellSize = w / cpr;
+		// 3. Initialize the cell array to the appropriate size.
+		cells = new Cell[cpr][cpr];
+		// 3. Iterate through the array and initialize each cell.
+		// Don't forget to consider the cell's dimensions when
+		// passing in the location.
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				cells[i][j] = new Cell(i * cellSize, j * cellSize, cellSize);
 			}
-		}else if(e.getSource() == randomizeButton) {
-			gamePanel.randomizeCells();
-		}else if(e.getSource() == clearButton) {
-			gamePanel.clearCells();
 		}
 	}
+
+	public void randomizeCells() {
+		// 4. Iterate through each cell and randomly set each
+		// cell's isAlive memeber to true of false
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				Random rand = new Random();
+
+				cells[i][j].isAlive = rand.nextBoolean();
+
+			}
+		}
+		repaint();
+	}
+
+	public void clearCells() {
+		// 5. Iterate through the cells and set them all to dead.
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				cells[i][j].isAlive = false;
+			}
+		}
+		repaint();
+	}
+
+	public void startAnimation() {
+		timer.start();
+	}
+
+	public void stopAnimation() {
+		timer.stop();
+	}
+
+	public void setAnimationDelay(int sp) {
+		timer.setDelay(sp);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// 6. Iterate through the cells and draw them all
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				cells[i][j].draw(g);
+			}
+		}
+
+		// draws grid
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+	}
+
+	// advances world one step
+	public void step() {
+		// 7. iterate through cells and fill in the livingNeighbors array
+		// . using the getLivingNeighbors method.
+		int[][] livingNeighbors = new int[cellsPerRow][cellsPerRow];
+
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				livingNeighbors[i][j] = getLivingNeighbors(i, j);
+			}
+		}
+
+		// 8. check if each cell should live or die
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				cells[i][j].liveOrDie(livingNeighbors[i][j]);
+			}
+		}
+
+		repaint();
+	}
+
+	// 9. Complete the method.
+	// It returns an int of 8 or less based on how many
+	// living neighbors there are of the
+	// cell identified by x and y
+	public int getLivingNeighbors(int x, int y) {
+		int neighbors = 0;
+
+		for (int i = x - 1; i <= x + 1; i++) {
+			for (int j = y - 1; j <= y + 1; j++) {
+				if (i >= 0 && j >= 0 && i < cells.length && j < cells[i].length) {
+					if (i == x && j == y) {
+						continue;
+					}
+
+					else if (cells[i][j].isAlive == true) {
+						neighbors++;
+					}
+				}
+
+			}
+		}
+
+		return neighbors;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// 10. Use e.getX() and e.getY() to determine
+		// which cell is clicked. Then toggle
+		// the isAlive variable for that cell.
+		cells[e.getX() / cellSize][e.getY() / cellSize].isAlive = true;
+
+		repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		step();
+	}
 }
+
--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -23,57 +23,92 @@ public class MazeMaker{
 		
 		//4. select a random cell to start
 		
-		
+		int cel = randGen.nextInt();
 		//5. call selectNextPath method with the randomly selected cell
-		
+		selectNextPath(uncheckedCells.get(cel));
 		
 		return maze;
 	}
 
 	//6. Complete the selectNextPathMethod
 	private static void selectNextPath(Cell currentCell) {
+		Cell celll;
+		Cell popedCell;
 		//A. mark cell as visited
-
+		currentCell.setBeenVisited(true);
 		//B. Get an ArrayList of unvisited neighbors using the current cell and the method below
-		
+		ArrayList<Cell> unvisitors = getUnvisitedNeighbors(currentCell);
 		//C. if has unvisited neighbors,
-		
+		if (!unvisitors.isEmpty()) {
+	
 			//C1. select one at random.
-			
+			celll = unvisitors.get(randGen.nextInt());
 			//C2. push it to the stack
-		
+			uncheckedCells.push(celll);
 			//C3. remove the wall between the two cells
-
+			removeWalls(currentCell, celll);
 			//C4. make the new cell the current cell and mark it as visited
-		
+		currentCell = celll;
+		currentCell.setBeenVisited(true);
+			
 			//C5. call the selectNextPath method with the current cell
-			
-			
+			selectNextPath(currentCell);
+		}	
 		//D. if all neighbors are visited
 		
+		if (unvisitors.isEmpty()) {
 			//D1. if the stack is not empty
+			if (!uncheckedCells.isEmpty()) {
 			
 				// D1a. pop a cell from the stack
-		
+			popedCell =	uncheckedCells.pop();
 				// D1b. make that the current cell
-		
+				popedCell = currentCell;
 				// D1c. call the selectNextPath method with the current cell
-				
-			
-		
+				selectNextPath(currentCell);
+			}
+		}
 	}
 
 	//7. Complete the remove walls method.
 	//   This method will check if c1 and c2 are adjacent.
 	//   If they are, the walls between them are removed.
 	private static void removeWalls(Cell c1, Cell c2) {
+		if (c1.getX()-c2.getX() > 0) {
+			c1.setEastWall(false);
+			c2.setWestWall(false);
+			
+		}
 		
+		if (c1.getY()-c2.getY() > 0) {
+			c1.setNorthWall(false);
+			c2.setSouthWall(false);
+		}
+		else if (c1.getX()-c2.getX() < 0) {
+			c1.setWestWall(false);
+			c2.setEastWall(false);
+			
+		}
+		
+		else if (c1.getY()-c2.getY() < 0) {
+			c1.setSouthWall(false);
+			c2.setNorthWall(false);
+		}
+	
 	}
+	
 	
 	//8. Complete the getUnvisitedNeighbors method
 	//   Any unvisited neighbor of the passed in cell gets added
 	//   to the ArrayList
 	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
+		if (c == null) {
+			return null;
+		}
+		else if (c != null) {
+			getUnvisitedNeighbors(c).add(c);
+		}
 		return null;
 	}
 }
+
